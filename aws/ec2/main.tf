09be5de212e64fe0db91950d56598d5065d2f989
Jonathan Meier <meierj@mbp14inch2021.myfiosgateway.com>
2021-11-01 08:12:51 -0400

    Only allow my IP

--- aws/ec2/main.tf
+++ aws/ec2/main.tf
@@ -41,8 +41,8 @@ resource "aws_security_group" "my_good_security_group" {
       from_port        = 22
       to_port          = 22
       protocol         = "tcp"
-      # cidr_blocks      = ["72.69.59.113/32"]
-      cidr_blocks      = ["0.0.0.0/0"]
+      cidr_blocks      = ["72.69.59.113/32"]
+      # cidr_blocks      = ["0.0.0.0/0"]
       ipv6_cidr_blocks = []
       prefix_list_ids  = []
       security_groups  = []
