2ecf3f39fda3fcbbc6163d2fcef6ccdc5ffe9287
Jonathan Meier <jonathanwmeier@gmail.com>
2021-11-19 00:01:08 -0500

    Very rough draft sync command
    really need to find a better way to do this.

--- aws/ec2/scripts/sync.sh
+++ aws/ec2/scripts/sync.sh
@@ -1,3 +1,4 @@
-#!/bin/bash -e
+#!/bin/bash -e -x
 
-rsync -avz ~/Projects $(terraform output meierj-remote-uri):/home/ubuntu/Projects
+$(terraform output --raw meierj-ssh-command) "mkdir -p /home/ubuntu/Projects/$1"
+rsync -avz ~/Projects/$1 $(terraform output --raw meierj-remote-uri):/home/ubuntu/Projects/$1
